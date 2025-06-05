9f47b1a4ab6114a55c7690e1db865ceb3a8eebd6
bingosdany <tartaraexpressalvajadas@gmail.com>
2025-06-04 22:32:52 -0500

    ✅ Añadidos campos tipo, cantidad y comprobante al esquema de Reserva

--- models/Reserva.js
+++ models/Reserva.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const reservaSchema = new mongoose.Schema({
   nombre: String,
   correo: String,
+  cantidad: String,
+  tipo: String,
+  comprobante: String,
   fecha: {
     type: Date,
     default: Date.now
+  },
+  validado: {
+    type: Boolean,
+    default: false
   }
 });
 
